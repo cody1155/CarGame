fa147352a67e8e9c9899ed6b078e7fb7b306ea8f
cody1155 <cody1155@gmail.com>
2019-06-13 09:44:46 -0700

    Added movement and boundaries

--- CarGame.py
+++ CarGame.py
@@ -17,15 +17,42 @@ pygame.draw.rect(screen, black, (80, 0, 110, 600))
 pygame.draw.rect(screen, black, (195, 0, 110, 600))
 pygame.draw.rect(screen, black, (310, 0, 110, 600))
 
+x = 0
+y = 0
+width = 64
+height = 64
+vel = 5
+
 game = True
 
 while game:
+    screen.fill(yellow)
+    pygame.draw.rect(screen, green, (0, 0, 75, 600))
+    pygame.draw.rect(screen, green, (425, 0, 75, 600))
+    pygame.draw.rect(screen, black, (80, 0, 110, 600))
+    pygame.draw.rect(screen, black, (195, 0, 110, 600))
+    pygame.draw.rect(screen, black, (310, 0, 110, 600))
 
     pygame.time.delay(100)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game = False
 
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_LEFT] and x > 0:
+        x -= vel
+
+    if keys[pygame.K_RIGHT] and x < 500 - width - vel:
+        x += vel
+
+    if keys[pygame.K_UP] and y > 0:
+        y -= vel
+
+    if keys[pygame.K_DOWN] and y < 600 - height - vel:
+        y += vel
+
+    pygame.draw.rect(screen, red, (x, y, width, height))
     pygame.display.update()
 
 
