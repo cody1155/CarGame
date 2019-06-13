8fbfebfc7fbd9a59a23fdbf0185b8ee6b14d1bd1
cody1155 <cody1155@gmail.com>
2019-06-13 10:59:30 -0700

    added car sprite and movement boundaries

--- CarGame.py
+++ CarGame.py
@@ -10,15 +10,10 @@ green = (0, 128, 0)
 yellow = (255, 255, 0)
 red = (255, 0, 0)
 
-screen.fill(yellow)
-pygame.draw.rect(screen, green, (0, 0, 75, 600))
-pygame.draw.rect(screen, green, (425, 0, 75, 600))
-pygame.draw.rect(screen, black, (80, 0, 110, 600))
-pygame.draw.rect(screen, black, (195, 0, 110, 600))
-pygame.draw.rect(screen, black, (310, 0, 110, 600))
+car = pygame.image.load('car2.png')
 
-x = 0
-y = 0
+x = 85
+y = 470
 width = 64
 height = 64
 vel = 5
@@ -40,19 +35,15 @@ while game:
 
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_LEFT] and x > 0:
-        x -= vel
+    if keys[pygame.K_LEFT] and x >= 100:
+        x -= 117
 
-    if keys[pygame.K_RIGHT] and x < 500 - width - vel:
-        x += vel
+    if keys[pygame.K_RIGHT] and x < 300:
+        x += 117
 
-    if keys[pygame.K_UP] and y > 0:
-        y -= vel
 
-    if keys[pygame.K_DOWN] and y < 600 - height - vel:
-        y += vel
-
-    pygame.draw.rect(screen, red, (x, y, width, height))
+    #pygame.draw.rect(screen, red, (x, y, width, height))
+    screen.blit(car, (x, y))
     pygame.display.update()
 
 
